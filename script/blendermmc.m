e76590957ed59b788b59f1284ac9047d8918071f
Qianqian Fang <fangqq@gmail.com>
2022-01-08 18:14:39 -0500

    add manual mesh retesselation for widefield area sources

--- script/blendermmc.m
+++ script/blendermmc.m
@@ -15,33 +15,36 @@ dir = R*[0;0;-1];
 
 %% cfg build
 cfg.nphoton= double(param.cfg.nphoton);
-cfg.gpuid=-1;
-cfg.unitinmm = param.cfg.unitinmm;
-cfg.node = meshdata.node;
-cfg.elem = meshdata.elem(:,1:4);
-cfg.elemprop=meshdata.elem(:,5);
 cfg.srctype=param.cfg.srctype;
 cfg.srcpos=param.cfg.srcpos;
 cfg.srcdir= dir';
 cfg.srcparam1=param.cfg.srcparam1;
 cfg.srcparam2=param.cfg.srcparam2;
 cfg.prop= prop;
+cfg.node = meshdata.node;
+cfg.elem = meshdata.elem(:,1:4);
+cfg.elemprop=meshdata.elem(:,5);
 cfg.tstart=0;
 cfg.tend=param.cfg.tend;
 cfg.tstep=param.cfg.tstep;
-cfg.debuglevel=param.cfg.debuglevel;
-cfg.issaveref=0;
-cfg.method=param.cfg.method;
-cfg.outputtype=param.cfg.outputtype;
 cfg.isreflect=double(param.cfg.isreflect);
 cfg.isnormalized=param.cfg.isnormalized;
 cfg.gpuid=param.cfg.gpuid;
+cfg.unitinmm = param.cfg.unitinmm;
+cfg.outputtype=param.cfg.outputtype;
+cfg.debuglevel=param.cfg.debuglevel;
+cfg.method=param.cfg.method;
 
 if(strcmp(cfg.srctype,'pencil') || strcmp(cfg.srctype,'isotropic') || strcmp(cfg.srctype,'cone'))
     cfg.e0=tsearchn(cfg.node,cfg.elem,cfg.srcpos);
     if(strcmp(cfg.srctype,'pencil') && isnan(cfg.e0))
         cfg.e0 = '-';
     end
+else
+    srcdef=struct('srctype',cfg.srctype,'srcpos',cfg.srcpos,'srcdir',cfg.srcdir,...
+         'srcparam1',cfg.srcparam1,'srcparam2',cfg.srcparam2);
+    [cfg.node,cfg.elem] = mmcaddsrc(cfg.node,[cfg.elem cfg.elemprop],...
+          mmcsrcdomain(srcdef,[min(cfg.node);max(cfg.node)]));
 end
 
 save('-mat7-binary',bpmwpath('mmccfg.mat'),'cfg');
